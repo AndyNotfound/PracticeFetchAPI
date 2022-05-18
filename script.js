0ba733743be6eca3ca296ee270612f6a110822ee
AndyNotfound <Andy.Notfound@gmail.com>
2022-05-19 01:27:05 +0700

    Damn the character won't show up

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ function displayQuotes (quoteTaken){
     // For the quotes tittle
     const tittle = quoteTaken.anime;
     const tittleDiv = document.getElementById('tittle');
-    const theTittle = tittle.anime;
     const tittleEl = document.createElement('h5');
     tittleEl.innerHTML = tittle;
     tittleDiv.className="text-gray-900 text-xl leading-tight font-medium mb-4";
@@ -26,9 +25,16 @@ function displayQuotes (quoteTaken){
     // For the quotes paragraph
     const word = quoteTaken.quote;
     const wordDiv = document.getElementById('someword');
-    const theQuotes = word.quote;
     const paragraph = document.createElement('p');
     paragraph.innerHTML = word;
     wordDiv.className="text-gray-700 text-base mb-4";
     wordDiv.appendChild(paragraph);
+
+    // For the quotes character
+    const character = quoteTaken.character;
+    const charDiv = document.getElementById('character');
+    const char = document.createElement('p');
+    char.innerHTML = character;
+    charDiv.className="text-gray-700 text-base mb-4";
+    charDiv.appendChild(character);
 };
