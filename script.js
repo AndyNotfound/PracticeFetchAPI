88b463d1cb0ed1469d8c7a4a7819f73f9f3033d2
AndyNotfound <Andy.Notfound@gmail.com>
2022-05-19 01:14:15 +0700

    Done!

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const insertHere = document.getElementById('#quote');
-
 fetch('https://animechan.vercel.app/api/random')
       .then((response) => {
           if (response.ok){
@@ -16,10 +14,21 @@ fetch('https://animechan.vercel.app/api/random')
       .catch((e) => console.log('Fetch Error:', e));
 
 function displayQuotes (quoteTaken){
+    // For the quotes tittle
+    const tittle = quoteTaken.anime;
+    const tittleDiv = document.getElementById('tittle');
+    const theTittle = tittle.anime;
+    const tittleEl = document.createElement('h5');
+    tittleEl.innerHTML = tittle;
+    tittleDiv.className="text-gray-900 text-xl leading-tight font-medium mb-4";
+    tittleDiv.appendChild(tittleEl);
+
+    // For the quotes paragraph
     const word = quoteTaken.quote;
     const wordDiv = document.getElementById('someword');
     const theQuotes = word.quote;
     const paragraph = document.createElement('p');
     paragraph.innerHTML = word;
+    wordDiv.className="text-gray-700 text-base mb-4";
     wordDiv.appendChild(paragraph);
 };
